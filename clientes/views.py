72bfff42e23f5d32c3afce517c6aee1bc4f55835
10Robert <robertlucasmtz124@gmail.com>
2023-12-07 15:49:02 -0300

    adcionando cache na api

--- clientes/views.py
+++ clientes/views.py
@@ -2,6 +2,8 @@ from rest_framework import viewsets, filters
 from clientes.serializers import ClienteSerializer
 from clientes.models import Cliente
 from django_filters.rest_framework import DjangoFilterBackend
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 
 class ClientesViewSet(viewsets.ModelViewSet):
     """Listando clientes"""
@@ -11,3 +13,8 @@ class ClientesViewSet(viewsets.ModelViewSet):
     ordering_fields  = ['nome']
     search_fields = ['nome', 'id', 'cpf']     
     filterset_fields = ['ativo']
+
+    
+    @method_decorator(cache_page(60 * 2))
+    def dispatch(self, *args, **kwargs):
+        return super(ClientesViewSet, self).dispatch(*args, **kwargs)
